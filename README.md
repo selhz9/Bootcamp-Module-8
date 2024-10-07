191a8236e864f5a9a78d61baba1b1a807fc81e5d
selhz9 <sarahhantz9@gmail.com>
2024-10-07 18:44:40 -0500

    Add existing files

---
+++ README.md
@@ -0,0 +1,32 @@
+# Module-8-Challenge
+Homework assignment for Coding Bootcamp 2024
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributors](#contributors)
+- [License](#license)
+- [Contact Me](#contact-me)
+
+## Description
+Vehicle Builder:
+This Challenge prompts the user to either create a new vehicle or select an existing vehicle. After going through the creation or selection process, the user is able to perform certain actions with the selected vehicle. The user is returned to the actions menu after each action until they decide to exit the application.
+
+## Installation
+Install node.js and run npm i
+
+## Usage
+This application builds and uses cars to include additional options for motorbikes and trucks.
+
+## Contributors
+This code and assignment description was given to me to modify by my Bootcamp 2024 Instructor.
+
+## License
+None
+
+## Questions
+Feel free to reach out to me here with any questions or comments:
+- Email: selhz@email.com
+- GitHub: selhz
+- Link to video:
---
+++ dist/classes/Motorbike.js
@@ -0,0 +1,47 @@
+// Importing Vehicle and Wheel classes
+import Vehicle from './Vehicle.js';
+import Wheel from './Wheel.js';
+// TODO: The Motorbike class should extend the Vehicle class
+class Motorbike extends Vehicle {
+    // TODO: Create a constructor that accepts the properties of the Motorbike class
+    constructor(vin, color, make, model, year, weight, topSpeed, wheels) {
+        // TODO: The constructor should call the constructor of the parent class, Vehicle
+        super();
+        // TODO: The constructor should initialize the properties of the Motorbike class
+        this.vin = vin;
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.weight = weight;
+        this.topSpeed = topSpeed;
+        // Check if the wheels array has 2 elements
+        if (wheels.length !== 2) {
+            this.wheels = [new Wheel(), new Wheel()];
+        }
+        else {
+            this.wheels = wheels;
+        }
+    }
+    // TODO: Implement the wheelie method
+    // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+    wheelie() {
+        console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+    }
+    // TODO: Override the printDetails method from the Vehicle class
+    printDetails() {
+        // TODO: The method should call the printDetails method of the parent class
+        super.printDetails();
+        // TODO: The method should log the details of the Motorbike
+        console.log(`VIN: ${this.vin}`);
+        console.log(`Make: ${this.make}`);
+        console.log(`Model: ${this.model}`);
+        console.log(`Year: ${this.year}`);
+        console.log(`Weight: ${this.weight} lbs`);
+        console.log(`Top Speed: ${this.topSpeed} mph`);
+        console.log(`Color: ${this.color}`);
+        console.log(`Wheels: ${this.wheels.length}`);
+    }
+}
+// Export the Motorbike class as the default export
+export default Motorbike;
